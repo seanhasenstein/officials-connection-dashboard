9a17175921bbb27f789b51a0c0f970268cf536ee
Sean Hasenstein <seanhasenstein@gmail.com>
2025-06-11 21:39:33 -0500

    add WIAA by session 3 times

---
+++ hooks/useDownloadWiaaBySessionThreeTimes.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import { createIdNumber } from '../utils/misc';
+
+import { currentYearString } from 'constants/currentYear';
+
+export default function useDownloadWiaaBySession() {
+  const wiaaSessionThreeTimesRef = React.useRef<HTMLAnchorElement>(null);
+
+  const handleDownloadWiaaBySessionThreeTimes = async () => {
+    const response = await fetch('api/download-wiaa-by-session-three-times');
+    if (!response.ok) {
+      throw new Error(
+        'Failed to fetch the WIAA form by session three times csv file.'
+      );
+    }
+
+    const data = await response.json();
+
+    wiaaSessionThreeTimesRef.current?.setAttribute(
+      'href',
+      `data:text/csv;charset=utf-8,${data.csv}`
+    );
+    wiaaSessionThreeTimesRef.current?.setAttribute(
+      'download',
+      `${currentYearString}-wiaa-by-session-three-times-${createIdNumber()}.csv`
+    );
+    wiaaSessionThreeTimesRef.current?.click();
+  };
+
+  return { handleDownloadWiaaBySessionThreeTimes, wiaaSessionThreeTimesRef };
+}
